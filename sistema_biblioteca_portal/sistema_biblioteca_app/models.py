2d9f9254def2d1f8d5aac5b94d5a92ba96654374
machinealpha <mdcn.hh@gmail.com>
2024-05-23 10:33:45 -0400

    Actualizacion 8

--- sistema_biblioteca_portal/sistema_biblioteca_app/models.py
+++ sistema_biblioteca_portal/sistema_biblioteca_app/models.py
@@ -5,5 +5,5 @@ class lectores(models.Model):
     matricula=models.CharField(max_length=200)
     nombre=models.CharField(max_length=200)
     curso=models.CharField(max_length=200)
-    correo=models.TextField()
+    observaciones=models.TextField()
     active=models.BooleanField(default=True)
