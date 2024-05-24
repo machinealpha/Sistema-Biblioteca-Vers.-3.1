c23fef8a064622fdc70667f264eeaa417e12f917
machinealpha <mdcn.hh@gmail.com>
2024-05-24 12:19:29 -0400

    Iteracion 3

--- sistema_biblioteca_portal/sistema_biblioteca_app/views.py
+++ sistema_biblioteca_portal/sistema_biblioteca_app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-#from django.contrib import render
+from django.contrib import admin
 from django.http import HttpResponse
 from django.shortcuts import render,redirect
 
 
 # Create your views here.
+from .models import *
+
 def home(request):
     return render(request,"home.html",context={"current_tab":"home"})
 
@@ -17,3 +19,7 @@ def reservaciones(request):
 def save_usuario(request):
     nombre_usuario = request.POST['nombre_usuario']
     return render(request,"welcome.html",context={'nombre_usuario':nombre_usuario})
+
+def lectores_tab(request):
+    lectores = lector.objects.all()
+    return render(request, "lectores.html",context={"current_tab": "lectores","lectores":lectores})
