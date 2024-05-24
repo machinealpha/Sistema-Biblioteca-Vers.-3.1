bee573c2cec6c4c4c1693cc37fa7baae758cbb99
machinealpha <mdcn.hh@gmail.com>
2024-05-23 23:35:02 -0400

    Actualizacion 9

--- sistema_biblioteca_portal/sistema_biblioteca_app/models.py
+++ sistema_biblioteca_portal/sistema_biblioteca_app/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 # Create your models here.
 class lectores(models.Model):
+    def __str__(self):
+        return self.lector
     matricula=models.CharField(max_length=200)
-    nombre=models.CharField(max_length=200)
+    lector=models.CharField(max_length=200)
     curso=models.CharField(max_length=200)
     observaciones=models.TextField()
     active=models.BooleanField(default=True)
